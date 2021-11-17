a1e813738ddba852b75a71859f8746272b0166d7
Jesper Paulsen <jesper@jesper.no>
2021-11-17 15:28:08 +0100

    remove meta tag

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,28 +1,25 @@
-import React from 'react'
-import Footer from '../footer'
-import Header from '../header/header'
-import Meta from '../meta'
-import PreviewWarning from '../previewWarning/previewWarning'
-
+import React from "react";
+import Footer from "../footer";
+import Header from "../header/header";
+import PreviewWarning from "../previewWarning/previewWarning";
 
 interface LayoutProps {
-  preview: boolean
+  preview: boolean;
 }
 
 const Layout: React.FC<LayoutProps> = ({ preview, children }) => {
   return (
     <>
-      <Meta />
-        <div className="min-w-screen">
-          {preview && <PreviewWarning />}
-          <Header />
-        </div>
-        <div className="min-h-screen w-screen">
-          <main>{children}</main>
-        </div>
+      <div className="min-w-screen">
+        {preview && <PreviewWarning />}
+        <Header />
+      </div>
+      <div className="min-h-screen w-screen">
+        <main>{children}</main>
+      </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
