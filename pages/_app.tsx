46ac55befb5f11e5d5d67a8f21ebc9a573205620
Jesper Paulsen <jesper@jesper.no>
2021-11-17 15:26:19 +0100

    update og description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,13 +12,18 @@ function StrahlCapital({ Component, pageProps }) {
       </Head>
       <DefaultSeo
         title="Strahl Capital"
-        description="Fuel for impact"
+        description="Fuel for impact.
+        There is no lack of important challenges to solve.
+        We support teams seeking to make a positive impact for the benefit of people and the planet."
         openGraph={{
           type: "website",
           locale: "en_IE",
           url: "https://www.strahl.no/",
           site_name: "Strahl Capital",
           images: [{ url: "/strahl.png" }],
+          description: `Fuel for impact.
+          There is no lack of important challenges to solve.
+          We support teams seeking to make a positive impact for the benefit of people and the planet.`,
         }}
       />
       <Layout preview={pageProps.preview}>
