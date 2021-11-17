24d83a3b8b2d418a181993456eb59404f33b4fd7
Jesper Paulsen <jesper@jesper.no>
2021-11-17 15:04:28 +0100

    work on SEO

--- components/grid/grid.tsx
+++ components/grid/grid.tsx
@@ -1,30 +1,37 @@
-import React from 'react'
-import DefaultImage from '../../types/DefaultImage'
-import Card from '../card/card'
+import React from "react";
+import DefaultImage from "../../types/DefaultImage";
+import Card from "../card/card";
 
 interface GridProps {
-  slugPrefix?: string
-  title?: string
-  large?: boolean
+  slugPrefix?: string;
+  title?: string;
+  large?: boolean;
   items: {
-    title: string,
-    subtitle: string,
-    image: DefaultImage,
-    href: string
-  }[]
+    title: string;
+    subtitle: string;
+    image: DefaultImage;
+    href: string;
+  }[];
 }
 
 const Grid: React.FC<GridProps> = ({ items, title, slugPrefix, large }) => {
-  return <div className="w-full">
-    {title && <div className="text-heading-xl pt-10">
-      {title.toUpperCase()}
-    </div>}
-      <div className={`grid ${large ? 'grid-cols-1 sm:grid-cols-2' :'grid-cols-2'} xl:grid-cols-4 gap-5 pt-4 w-max`}>
+  return (
+    <div className="w-full">
+      {title && (
+        <div className="text-heading-xl pt-10">{title.toUpperCase()}</div>
+      )}
+      <div className={`grid grid-cols-2 xl:grid-cols-4 gap-5 pt-4 w-max`}>
         {items.map((item, i) => (
-          <Card key={`card-${i}`} {...item} slugPrefix={slugPrefix} large={large} />
+          <Card
+            key={`card-${i}`}
+            {...item}
+            slugPrefix={slugPrefix}
+            large={large}
+          />
         ))}
       </div>
-  </div> 
-}
+    </div>
+  );
+};
 
-export default Grid
+export default Grid;
--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,14 +1,14 @@
-import { InferGetStaticPropsType } from 'next'
-import Head from 'next/head'
-import React, { useState } from 'react'
-import BlockContentWrapper from '../../components/blockContentWrapper/blockContentWrapper'
-import Container from '../../components/container/container'
-import Grid from '../../components/grid/grid'
-import Prose from '../../components/prose/prose'
-import SanityPageService from '../../services/SanityPageService'
-import AboutPage from '../../types/AboutPage'
-import InvestmentsPage from '../../types/InvestmentsPage'
-import NewsPage from '../../types/NewsPage'
+import { InferGetStaticPropsType } from "next";
+import Head from "next/head";
+import React, { useState } from "react";
+import BlockContentWrapper from "../../components/blockContentWrapper/blockContentWrapper";
+import Container from "../../components/container/container";
+import Grid from "../../components/grid/grid";
+import Prose from "../../components/prose/prose";
+import SanityPageService from "../../services/SanityPageService";
+import AboutPage from "../../types/AboutPage";
+import InvestmentsPage from "../../types/InvestmentsPage";
+import NewsPage from "../../types/NewsPage";
 
 const query = `*[_type == "news"][0] {
   title,
@@ -19,19 +19,21 @@ const query = `*[_type == "news"][0] {
     image,
     "href": slug.current
   }
-}`
+}`;
 
-const pageService = new SanityPageService<NewsPage>(query)
+const pageService = new SanityPageService<NewsPage>(query);
 
-const News: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = (context) => {
-  const { data } = pageService.getPreviewHook(context)()
+const News: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = (
+  context
+) => {
+  const { data } = pageService.getPreviewHook(context)();
 
   return (
     <>
       <Head>
         <title>{data.title}</title>
       </Head>
-      <div className="pt-8 flex">
+      <div className="pt-8">
         <Container>
           <div className="text-3xl md:text-6xl xl:text-7xl md:leading-tight font-semibold">
             {data.title}
@@ -40,21 +42,16 @@ const News: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = (context)
             <BlockContentWrapper text={data.description} />
           </Prose>
           <div className="w-full max-w-5xl">
-            <Grid
-              large
-              slugPrefix='/news'
-              items={data.news}
-            />
+            <Grid large slugPrefix="/news" items={data.news} />
           </div>
         </Container>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default News
+export default News;
 
 export function getStaticProps(context) {
-  return pageService.fetchQuery(context)
+  return pageService.fetchQuery(context);
 }
-
