63f73073704d58b3420471ef9993aa192411c0e1
Jesper Paulsen <jesper@jesper.no>
2022-01-19 20:13:03 +0100

    use mailersend for emails

--- pages/api/send-message.tsx
+++ pages/api/send-message.tsx
@@ -1,46 +1,41 @@
-import nodemailer from 'nodemailer'
+import axios from "axios";
 
-console.log(process.env.GMAIL_EMAIL,process.env.GMAIL_PASSWORD,)
-
-const transporter = nodemailer.createTransport({
-  port: 465,
-  host: "smtp.gmail.com",
-  auth: {
-    user: process.env.GMAIL_EMAIL,
-    pass: process.env.GMAIL_PASSWORD,
-  },
-  secure: true,
-})
+const doRequest = async (email?: any) => {
+  const url = "https://api.mailersend.com/v1/email";
+  const token = process.env.MAILERSEND_TOKEN;
+  if (!token) throw new Error("No MailerSend key provided");
+  const res = await axios.post(url, JSON.stringify(email), {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+  return res.data;
+};
 
 const sendMessage = async (req, res) => {
-  const { email, name, message } = req.body
-
-  const mailData = {
-    from: process.env.GMAIL_EMAIL,
-    to: 'luisp@eunet.no',
+  const { email: emailAdress, name, message } = req.body;
+  const email = {
+    from: {
+      email: "jesper@jesper.no",
+      name: `Strahl Capital | ${emailAdress}`,
+    },
+    to: [
+      {
+        email: "luisp@eunet.no",
+      },
+    ],
     subject: `Message to Strahl Capital From ${name}`,
-    text: message + " | Sent from: " + email,
-    html: `<div>${message}</div><p>Sent from:
-    ${email}</p>`
-  }
+    text: `The following was sent from ${name}, email: ${emailAdress}.\n---------------------------\n${message}`,
+  };
+
   try {
-    await sendEmail(mailData)
-    return res.status(200).send()
+    await doRequest(email);
+    return res.status(200).send();
   } catch (e) {
-    console.error(e)
-    return res.status(500).send()
+    console.error(e);
+    return res.status(500).send();
   }
-}
+};
 
-const sendEmail = async(mailData) => {
-  return new Promise((resolve, reject) => {
-    return transporter.sendMail(mailData, (err, info) => {
-      if(err)
-        return reject(err)
-      else
-        return resolve(info)
-    })
-  })
-}
-
-export default sendMessage
+export default sendMessage;
